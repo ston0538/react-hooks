1d477143d0217fcf4c44f7917c4af614e30437d1
KHK <ston0538@gmail.com>
2020-04-18 19:45:53 +0900

    useState 값변화

--- src/App.js
+++ src/App.js
@@ -2,39 +2,57 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  // prac 1
+  // const [count, setCount] = useState(10);
+
+  // prac 2
   // object로 한번에 2개의 값 변화 or 한개의 값 변화
-  // const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
   // 그냥 보통 2개로 나누어서 해도됨
+  // const [{ count1, count2 }, setCount] = useState({ count1: 10, count2: 20 });
+
+  // prac3
   const [count1, setCount1] = useState(10);
   const [count2, setCount2] = useState(20);
   return (
     <div>
-      {/* object 한개의 값만 업데이트 */}
+      {/* <button onClick={() => setCount(count + 1)}>prac1 +</button> */}
+      {/* <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        prac1 +
+      </button> */}
+
       {/* <button
         onClick={() =>
-          setCount(c => ({
-            ...c,
-            count1: c.count1 + 1
+          setCount((currentState) => ({
+            count1: currentState.count1 + 1,
+            count2: currentState.count2 + 1,
           }))
         }
-      > */}
-      {/* object 두개의 값 업데이트 */}
+      >
+        object 내에서 2개의 값 변화
+      </button> */}
+
       {/* <button
         onClick={() =>
-          setCount(c => ({
-            count1: c.count1 + 1,
-            count2: c.count2 + 1
+          setCount((currentState) => ({
+            ...currentState,
+            count1: currentState.count1 + 1,
+            // count2: currentState.count2 + 1,
           }))
         }
-      > */}
-      <button
+      >
+        object 내에세 1개의 값만 변화
+      </button> */}
+
+      {/* <button
         onClick={() => {
           setCount1((c) => c + 1);
           setCount2((c) => c + 1);
         }}
       >
-        +
-      </button>
+        각각 변화
+      </button> */}
+
+      {/* <div>{count}</div> */}
       <div>{count1}</div>
       <div>{count2}</div>
     </div>
