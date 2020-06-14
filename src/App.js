4cbbaa50103daaa181cb5fc74a74a30c85e3ab38
KHK <ston0538@gmail.com>
2020-06-15 01:31:24 +0900

    Done useReducer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import React, { useState, useMemo } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import { UserContext } from "./UserContext";
+import React, { useState, useReducer } from "react";
 
-function AppRouter() {
-  const [user, setUser] = useState(null);
-  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            {user && <Link to="/about/">About</Link>}
-          </ul>
-        </nav>
-        <UserContext.Provider value={value}>
-          <Route path="/" exact component={Index} />
-          <Route path="/about/" exact component={About} />
-        </UserContext.Provider>
-      </div>
-    </Router>
-  );
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-todo":
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1,
+      };
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((todo, index) =>
+          index === action.index
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+        todoCount: state.todoCount,
+      };
+    default:
+      return state;
+  }
 }
 
-export default AppRouter;
+const App = () => {
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0,
+  });
+  const [text, setText] = useState();
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "add-todo", text });
+          setText("");
+        }}
+      >
+        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+      </form>
+      {/* <pre>{JSON.stringify(todos, null, 2)}</pre> */}
+      <h3>Count: {todoCount}</h3>
+      {todos.map((todo, index) => (
+        <div
+          key={index}
+          onClick={() => dispatch({ type: "toggle-todo", index })}
+          style={{
+            textDecoration: todo.completed ? "line-through" : "",
+          }}
+        >
+          {todo.text}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
