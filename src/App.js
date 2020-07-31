51452ebbc6a685cb051b6ee2cb3216628325392e
KHK <ston0538@gmail.com>
2020-08-01 03:38:19 +0900

    Done useMemo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useMemo } from "react";
 import "./App.css";
 import Count from "./components/Count";
 
+const data = { isEven: false };
 const App = () => {
   const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
   const test = 1;
   const renders = useRef(0);
   // console.log(renders.current);
   const onOdd = useCallback(() => setText(""), [setText]);
+  const data = useMemo(
+    () => ({
+      text2,
+      isEven: text2.length % 2 === 0,
+    }),
+    [text2]
+  );
+  // docs 처럼 함수에 a,b인자가 사용되지 않았으니 종속성도 필요 없다.
+
   return (
     <div>
       <input value={text} onChange={(e) => setText(e.target.value)} />
+      <input
+        value={text2}
+        placeholder="text2"
+        onChange={(e) => setText2(e.target.value)}
+      />
       <div>App component renders: {renders.current++}</div>
-      <Count onOdd={onOdd} />
+      <Count onOdd={onOdd} data={data} />
     </div>
   );
 };
--- src/components/Count.js
+++ src/components/Count.js
@@ -1,25 +1,36 @@
 import React, { useState, useRef } from "react";
 
-const Count = React.memo(({ onOdd }) => {
-  const [count, setCount] = useState(0);
-  const renders = useRef(0);
-  return (
-    <div>
-      <h3>Child component</h3>
-      <div>count: {count}</div>
-      <div>Count component renders: {renders.current++}</div>
-      <button
-        onClick={() => {
-          if (count % 2 === 0) {
-            onOdd();
-          }
-          setCount((c) => c + 1);
-        }}
-      >
-        increment
-      </button>
-    </div>
-  );
-});
+const Count = React.memo(
+  ({ onOdd, data }) => {
+    const [count, setCount] = useState(0);
+    const renders = useRef(0);
+    console.log(data);
+    return (
+      <div>
+        <h3>Child component</h3>
+        <div>count: {count}</div>
+        <div>Count component renders: {renders.current++}</div>
+        <button
+          onClick={() => {
+            if (count % 2 === 0) {
+              onOdd();
+            }
+            setCount((c) => c + 1);
+          }}
+        >
+          increment
+        </button>
+      </div>
+    );
+  }
+  // (prevProps, nextProps) => {
+  //   if (prevProps.data.isEven !== nextProps.data.isEven) {
+  //     return false;
+  //   }
+  //   return true;
+  //   // true를 반환하면 랜더링 되지 않는다.
+  //   // isEven은 변화를 일으키지 않고 있다.
+  // }
+);
 
 export default Count;
